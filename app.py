805e14939b2dfe8f2e6a13fae1e1026087e977d6
AIbyML.com <aibyml@users.noreply.huggingface.co>
2023-12-09 13:07:20 +0000

    Update app.py

--- app.py
+++ app.py
@@ -11,26 +11,26 @@ if 'unique_id' not in st.session_state:
 def main():
     load_dotenv()
     st.set_page_config(page_title="Digital Assistant with AI Capability")
-    st.title("Assistant: Master - Welcome back ...💁 ")
-    st.subheader("Assistant: Master, please go through steps before processing your input knowledge")
+    st.title("J.A.R.V.I.S: Master - Welcome back ...💁 ")
+    st.subheader("J.A.R.V.I.S: Master, please go through steps before processing your input knowledge")
     
-    with st.form("Assistant: Master, please input Client PASSWORD"):
-        st.session_state['unique_id']= st.text_input("identity", value="", type="password")
+    with st.form("J.A.R.V.I.S: Master, please input Client PASSWORD"):
+        st.session_state['unique_id']= st.text_input("unique_id", value="", type="password")
         if st.form_submit_button("Submit"):
             st.session_state.prompt_history = []
             st.session_state.df = None
-            st.success('Assistant: Saved Authenticated Client identity in me')    
-    enquiry = st.text_area("Assitant: Please post the issues rasied by the client here...",key="1")
-    document_count = st.text_input("Assistant: Master, how many numbers of 'REFERENCE' you expect me to find to tackle the issue",key="2")
+            st.success('J.A.R.V.I.S: Saved Authenticated Client identity in me')    
+    enquiry = st.text_area("J.A.R.V.I.S: Please post the issues rasied by the client here...",key="1")
+    document_count = st.text_input("J.A.R.V.I.S: Master, how many numbers of 'REFERENCE' you expect me to find to tackle the issue",key="2")
     
     # Upload the Resumes (pdf files)
-    pdf = st.file_uploader("Assistant: Master, please upload knowledge material here, only PDF files allowed", type=["pdf"],accept_multiple_files=True)
+    pdf = st.file_uploader("J.A.R.V.I.S: Master, please upload knowledge material here, only PDF files allowed", type=["pdf"],accept_multiple_files=True)
 
-    submit=st.button("Assistant, please help me with the professional analysis")
+    submit=st.button("J.A.R.V.I.S, please help me with the professional analysis")
 
     if submit:
         with st.spinner('Wait for it...'):
-            st.write("Your client ID")
+            st.write("Client Unique ID")
             
             # Creating a unique ID, so that we can use to query and get only the user uploaded documents from PINECONE vector store
             #st.session_state['unique_id']=uuid.uuid4().hex
@@ -50,8 +50,8 @@ def main():
             # Push data to Vector Store
             db=push_to_store(embeddings,docs)
 
-            # Fecth relavant documents from Vector Store
-            relevant_docsvant_docs=get_similar_docs(enquiry,document_count,db, embeddings,st.session_state['unique_id'])
+            # Fectch relevant documents from Vector Store
+            relevant_docs=get_similar_docs(enquiry,document_count,db, embeddings,st.session_state['unique_id'])
 
             #st.write(relevant_docs)
 
@@ -77,7 +77,7 @@ def main():
                 combined_doc = combined_text(relevant_docs)
                 answer = answer_question(enquiry, combined_doc)
                 st.write(answer)
-                st.success("Assistant: Hope I am able to help your in respond to your client effectively ❤️ ")
+                st.success("J.A.R.V.I.S: Hope I am able to help you in respond to your client effectively ❤️ ")
 
 
 # Invoking main function
